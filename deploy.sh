2dd2b3b552d274075c9617317a4156db4de3d545
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-18 16:33:29 -0300

    Ajustes remoto II

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,25 @@
 cd terraform/
 
-echo "entrando no diretório do terraform"
+~/terraform  init init
 
-terraform init
-terraform validate
-terraform apply -auto-approve
+~/terraform  init validate
+
+~/terraform  init apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
+
 sleep 10 # 10 segundos
 
 echo $"[ec2-nodejs]" > ../ansible/hosts # cria arquivo
-echo "$(terraform output | awk '{print $3;exit}')" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+
+echo "$( ~/terraform  init output | awk '{print $3;exit}')" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
+
 sleep 10 # 20 segundos
 
 cd ../ansible
+
 ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa
 
 cd ../terraform
