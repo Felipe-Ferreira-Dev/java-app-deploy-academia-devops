5cee1d54400c8dcaec44834b4cfd2025ef6a1fc7
Felipe-Ferreira-Dev <felipe.ferreira.sco@gmail.com>
2021-10-18 20:08:08 +0000

    ajustes locais

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,21 @@
-cd terraform/ &
-
-~/terraform  init init &
-
-~/terraform  init validate &
-
-~/terraform  init apply -auto-approve &
+cd ~/projetos/java-app-deploy-academia-devops/terraform &
+pwd
+~/terraform init &
+~/terraform validate &
+~/terraform apply -auto-approve &
 
 echo "Aguardando criação de maquinas ..." &
 
 sleep 10 &
 
-echo $"[ec2-nodejs]" > ../ansible/hosts &
-
-echo "$( ~/terraform  init output | awk '{print $3;exit}')" >> ../ansible/hosts &
-
+echo "[ec2-lab-docker]" > ../ansible/hosts &
+echo "$(~/terraform output | awk '{print $3;exit}')" >> ../ansible/hosts &
 echo "Aguardando criação de maquinas ..." &
 
 sleep 10 &
 
 cd ../ansible &
 
-ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa &
+ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa 
 
 cd ../terraform
