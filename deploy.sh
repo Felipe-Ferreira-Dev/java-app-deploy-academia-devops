9c9ad37d4cdb8b20b22ede949024218a153324e3
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-18 16:16:50 -0300

    ajustes lógica

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
-cd java-app-deploy-academia-devopscd 
-docker build -t ffaneto/java-app-deploy-academia-devops -f Dockerfile .
-docker run -d -p 8081:8081 --name java-app-deploy-academia-devops ffaneto/java-app-deploy-academia-devops
-docker stop java-app-deploy-academia-devops
-
 cd terraform/
 terraform init
 terraform validate
@@ -12,7 +7,7 @@ echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
 
 echo $"[ec2-nodejs]" > ../ansible/hosts # cria arquivo
-echo "$(~/terraform/terraform output | awk '{print $3;exit}')" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(terraform output | awk '{print $3;exit}')" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 20 segundos
